aca2a4d96042ed28aa3d7f4dd4c69c1111151532
Abhishek K P <abhishekkp615@gmail.com>
2023-06-14 00:16:52 +0530

    final test

--- gfg/urls.py
+++ gfg/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
  
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
