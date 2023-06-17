2ab9ea5b96d414dc301d9df5290dd2bc12b90d73
Abhishek K P <abhishekkp615@gmail.com>
2023-06-17 14:13:59 +0530

    keystroke

--- gfg/settings.py
+++ gfg/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 from . info import *  # For email Confirmation
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
