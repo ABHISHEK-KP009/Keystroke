fc16c989f03fecf9b7135823b61a7e03559a7e35
Abhishek K P <abhishekkp615@gmail.com>
2023-06-21 02:09:51 +0530

    remove

--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from gfg import settings
+from Keystroke import settings
 from django.core.mail import send_mail
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError as DjangoValidationError
